7b889e1db759d19dad65a41c11afcc3f91a0e645
Enzo Tiezzi <enzo_tiezzi@hotmail.com>
2020-09-03 15:21:45 -0300

    using character controller instead of rigidy body

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float jumpSpeed = 8.0F;
+    public float gravity = 20.0F;
+    private Vector3 moveDirection = Vector3.zero;
+
+    public void Update()
+    {
+        var controller = GetComponent<CharacterController>();
+
+        if (controller.isGrounded && Input.GetButton("Jump"))
+        {
+            moveDirection.y = jumpSpeed;
+        }
+
+        moveDirection.y -= gravity * Time.deltaTime;
+        controller.Move(moveDirection * Time.deltaTime);
+    }
+}
